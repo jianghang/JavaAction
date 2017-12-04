31adf707222b8640190635420a3ac5c5da25c045
JiangHang <664019848@qq.com>
2017-12-04 21:47:12 +0800

    20171204

--- src/main/java/com/hangjiang/threads/assemblyline/PStreamMain.java
+++ src/main/java/com/hangjiang/threads/assemblyline/PStreamMain.java
@@ -2,6 +2,7 @@ package com.hangjiang.threads.assemblyline;
 
 /**
  * Created by jianghang on 2017/12/4.
+ * 该程序实现了一个算术的流水线，例如：((1000+1000)*1000)/2=1000000.0
  */
 public class PStreamMain {
     public static void main(String[] args){
